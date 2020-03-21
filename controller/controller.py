2b5f0a87bc8526c0aa49cdeca9e8ca97db633224
Miquel Miro Nicolau <miquelca32@gmail.com>
2020-03-21 17:22:25 +0100

    Now exist a decorator to check if the model exist.

--- controller/controller.py
+++ controller/controller.py
@@ -23,28 +23,41 @@ class Controller:
             self.__view.image.set_n_images(len(self.__model))
         self.__view.title(f"DICOM Reader - {filepath}")
 
+    def exist_model(self, func):
+        def wrapper():
+            if self.__model is None:
+                messagebox.showerror("Error", "No has carregat una imatge vàlida")
+            else:
+                func()
+
+        return wrapper
+
+    @exist_model
     def show_headers(self):
-        if self.__model is None:
-            messagebox.showerror("Error", "No has carregat una imatge vàlida")
-        else:
-            dades = []
-            #     for h, v in image.get_header():
+        dades = []
 
-            for h, v in self.__model.get_header():
-                str_v = str(v)
-                str_h = str(h)
-                if hasattr(v, 'length'):
-                    if v.length > 200:
-                        print(v.length)
-                        str_v = str_v[:400]
-                dades.append([str_h, str_v])
-            tktable.make_table("Capceleres", dades)
+        for h, v in self.__model.get_header():
+            str_v = str(v)
+            str_h = str(h)
+            if hasattr(v, 'length'):
+                if v.length > 200:
+                    print(v.length)
+                    str_v = str_v[:400]
+            dades.append([str_h, str_v])
+        tktable.make_table("Capceleres", dades)
 
+    @exist_model
     def change_depth(self, value):
         depth = int(value)
 
         if self.__model is not None and depth < len(self.__model):
             self.__view.image.show_image(self.__model[depth])
+
+    @exist_model
+    def change_zoom(self, value):
+        zoom = int(value)
+
+        self.__model.resize_factor = zoom
 
     def start(self):
         self.__view.draw()
