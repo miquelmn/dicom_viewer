74c98e8cf0d8dd05b08d810968df218ba8ff6cd0
Miquel Miro Nicolau <miquelca32@gmail.com>
2020-05-17 21:15:30 +0200

    Add documentation to main script

--- viewer/app.py
+++ viewer/app.py
@@ -1,8 +1,14 @@
+""" Main script
+
+"""
 from viewer.view import gui
 from viewer.controller.controller import Controller
 
 
 def run():
+    """ Main method
+
+    """
     vw = gui.View("DICOM Reader")
     contr = Controller(vw)
     contr.start()
